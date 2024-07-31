c8c6c3ba5182d56bd21be2364aca5675cadcad0a
Chiragshisode <chetankshisode92@gmail.com>
2024-07-31 07:53:48 +0530

    Javascript Variables
    Javascript Variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,6 @@
+
+console.log("My city name is : Amalner.")
+console.log("My best friend's name is : Devesh.")
+console.log("My country name is : India.")
+console.log("My school name is : Sane Guruji Nutan Madhyamic Vidyalaya.")
+console.log("My college name is : SSBT COET, Jalgaon.")
---
+++ 01-variable.js
@@ -0,0 +1,8 @@
+
+// Variable Declaration
+var firstName;
+console.log(firstName);
+
+// Variable Initialization
+firstName = "Chetan";
+console.log(firstName);
